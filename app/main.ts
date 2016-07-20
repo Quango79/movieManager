be2cc632efabd23e60b506d69e4859728a197716
Quango79 <quandt.patrick@googlemail.com>
2016-07-20 21:42:41 +0200

    initial commit

---
+++ app/main.ts
@@ -0,0 +1,7 @@
+/**
+ * Created by Lex on 7/20/2016.
+ */
+import { bootstrap }    from '@angular/platform-browser-dynamic';
+import { AppComponent } from './app.component';
+bootstrap(AppComponent);
+
